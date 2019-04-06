930b5d1fedc0c1fc956355fdcdf74abc6c7a5557
zhangyang <zhangyang@ec.e-chemchina.com>
2019-04-06 16:40:27 +0800

    自定义VUE插件的方法

--- VueTest/13_插件/vue-myPlugin.js
+++ VueTest/13_插件/vue-myPlugin.js
@@ -1,9 +1,11 @@
 (function (window) {
+  //MyPlugin 就是我们想要向外暴露的插件对象
   const MyPlugin = {}
+  //自定义的插件对象必须有一个install的方法
   MyPlugin.install = function (Vue, options) {
     // 1. 添加全局方法或属性
     Vue.myGlobalMethod = function () {
-      console.log('Vue函数对象的myGlobalMethod()')
+      console.log('Vue函数对象的方法myGlobalMethod()')
     }
 
     // 2. 添加全局资源
@@ -16,5 +18,6 @@
       console.log('vm $myMethod()')
     }
   }
+  // 向外暴露插件
   window.MyPlugin = MyPlugin
 })(window)
