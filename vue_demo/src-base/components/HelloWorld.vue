1904a7e2541f347cb43ea72bef80e73b477d1efb
zhangyang <969666310@qq.com>
2019-09-17 19:14:04 +0800

    VUE进度

---
+++ vue_demo/src-base/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+/* 向外默认暴露一个配置对象 与vue一致 */
+export default {
+  /* data可以写对象,也可以写函数,但是在组件里面只能写函数 */
+  data () {
+    return {
+      msg: 'Hello VUE Component'
+    }
+  }
+}
+</script>
+
+<style>
+  .msg {
+    color: red;
+    font-size: 30px;
+  }
+</style>
--- vue_demo/src-base/main.js
+++ vue_demo/src-base/main.js
@@ -1,9 +1,11 @@
+/* 入口js : 创建VUE的实例 */
 import Vue from 'vue'
-
 import App from './App'
 
 new Vue({
   el: '#app',
-  components: {App},
+  components: {
+    App
+  },
   template: '<App/>'
 })
