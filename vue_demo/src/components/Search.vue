3c7a8c0a28d67c6a48a84a16269cee130ba62820
zhangyang <969666310@qq.com>
2019-09-20 11:31:58 +0800

    axios发送ajax请求查找github用户

--- vue_demo/src/components/Search.vue
+++ vue_demo/src/components/Search.vue
@@ -1,10 +1,34 @@
 <template>
-
+  <div>
+    <section class="jumbotron">
+      <h3 class="jumbotron-heading">Search Github Users</h3>
+      <div>
+        <input type="text" placeholder="enter the name you search" v-model="searchName"/>
+        <button @click="searchUser">Search</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+  import PubSub from 'pubsub-js'
   export default {
-    name: 'Search'
+    name: 'Search',
+    data () {
+      return {
+        searchName: ''
+      }
+    },
+
+    methods: {
+      searchUser(){
+        const searchName = this.searchName.trim()
+        if(searchName){
+          // 发布搜索的消息
+          PubSub.publish('searchUser',searchName)
+        }
+      }
+    }
   }
 </script>
 
